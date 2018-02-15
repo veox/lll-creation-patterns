10953b916010b357ea4d903ce6f1a10e9b640750
Noel Maersk <veox+gits@veox.pw>
2018-02-15 16:24:40 +0200

    tests/cannery: remove DEBUG prints + factor out get_log_data().

--- tests/test_cannery.py
+++ tests/test_cannery.py
@@ -1,15 +1,25 @@
 import pytest
 from ethereum.tester import TransactionFailed
 
+# bytecode of *revert-guard* in cannery/can-opener contracts
+revertguard = '0x600080fd' # PUSH1 0x00 DUP1 REVERT
+
 def print_memdump(chain, txreceipt):
+    '''Hacky helper for memory inspection.'''
     print('-'*33 + 'MEMDMP' + '-'*33)
     data = txreceipt['logs'][0]['data']
     data = data[2:] # snip 0x
     for i in range(int(len(data)/64)):
         print(chain.web3.toHex(i*32), data[i*64:i*64+64], sep='\t')
 
-    # FIXME: required since accessing logs by index instead of event id
+    # FIXME: required since accessing logs by index instead of event id;
+    # also, forces the dump to be displayed
     assert False
+
+def get_log_data(txreceipt, logindex=0):
+    '''Extract ``data`` from log entry in the transaction receipt.'''
+    assert len(txreceipt['logs']) != 0
+    return txreceipt['logs'][logindex]['data']
 
 ########################################################################
 
@@ -30,31 +40,18 @@ def test_cannery(chain):
     txhash = chain.web3.eth.sendTransaction(transaction)
     txreceipt = chain.wait.for_receipt(txhash)
 
-    # DEBUG
-    print('transaction:', transaction)
-    print(txhash)
-    print(txreceipt)
-    print('-'*72)
-
     # get can's address from log
-    assert len(txreceipt['logs']) != 0
-    canaddr = chain.web3.toChecksumAddress(txreceipt['logs'][0]['data'])
+    canaddr = chain.web3.toChecksumAddress(get_log_data(txreceipt))
     assert canaddr != '0x0000000000000000000000000000000000000000'
 
     # see what's in the can (cheat!)
     canbytecode = chain.web3.eth.getCode(canaddr)
-    revertguard = '0x600080fd'
     # "normal" runtime bytecode is in the can
     assert bytecode[2:] in canbytecode[2:]
     # the can has a revert guard
     assert canbytecode.startswith(revertguard)
     # there is nothing else but the can and its contents
     assert canbytecode == revertguard + bytecode[2:]
-
-    # DEBUG
-    print('can', canaddr, 'bytecode:')
-    print(canbytecode)
-    print('-'*72)
 
     # attempt to call can directly (should REVERT with very low gas use)
     transaction = {
@@ -70,33 +67,18 @@ def test_cannery(chain):
     # it doesn't know of REVERT, so terminates as if INVALID was called
     #assert txreceipt['gasUsed'] < 21100
 
-    # DEBUG
-    print('transaction:', transaction)
-    print(txhash)
-    print(txreceipt)
-    print('-'*72)
-
+    # open the canned vegetable using the can-opener
     txhash = opener.transact().open(canaddr)
     txreceipt = chain.wait.for_receipt(txhash)
 
-    # print_memdump(chain, txreceipt) # DEBUG (uncomment here and in contract!)
-
-    # DEBUG
-    print(txhash)
-    print(txreceipt)
-    print('-'*72)
+    # DEBUG (uncomment here and in contract!)
+    # print_memdump(chain, txreceipt)
 
     # get uncanned contract's address from log
-    assert len(txreceipt['logs']) != 0
-    veggieaddr = chain.web3.toChecksumAddress(txreceipt['logs'][0]['data'])
-    assert veggieaddr != '0x0000000000000000000000000000000000000000'
+    myveggieaddr = chain.web3.toChecksumAddress(get_log_data(txreceipt))
+    assert myveggieaddr != '0x0000000000000000000000000000000000000000'
 
     # see what's in the veggie (cheat!)
-    veggiebytecode = chain.web3.eth.getCode(veggieaddr)
+    myveggiebytecode = chain.web3.eth.getCode(myveggieaddr)
     # uncanned vegetable's runtime bytecode matches that of never-canned
-    assert veggiebytecode == Vegetable.bytecode_runtime
-
-    # DEBUG
-    print('vegetable', veggieaddr, 'bytecode:')
-    print(veggiebytecode)
-    print('-'*72)
+    assert myveggiebytecode == Vegetable.bytecode_runtime
